e668f0a7391c7f04f01b36b7a4b78240a31455a6
Parshukov Danil <d.parshukov@potok.digital>
2019-05-18 18:53:25 +0600

    Description of api is added

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,81 @@
 module Api
   class MessagesController < ApplicationController
+    # POST /api/messages
+    # Создаёт сообщения для рассылки,
+    # после чего кладёт их в очередь для отправки в зависимости от времени
+    #
+    # Данные должны быть в формате json, в хедере запроса должно быть выставлено значение:
+    #   Content-Type - application/json
+    #
+    # Пример запроса:
+    #
+    # {"message":
+    #   {
+    #     "body":"hello my darling",
+    #     "consumers_info":[
+    #       {
+    #         "time_to_deliver": 1558181088,
+    #         "messenger_user_id": 2,
+    #         "messenger": "whats_app"
+    #       },
+    #       {
+    #         "messenger_user_id": 3,
+    #         "messenger": "whats_app"
+    #       }
+    #     ]
+    #   }
+    # }
+    #
+    #  Описание аттрибутов:
+    #    message/body - string. Обязательный параметр, текст сообщения длина может быть от 1 до 500
+    #
+    #    message/consumers_info - array. Обязательный параметр, массив из хешей, в котором указываются
+    #                             мессенджеры, пользователи и время для отправки сообщения
+    #
+    #    message/consumers_info/messenger_user_id - integer. Обязательный параметр, id пользователя
+    #    message/consumers_info/messenger - string. Обязательный параметр, мессенджер, куда отправляются
+    #                                       сообщения. Может быть равен 'viber', 'whatsapp', 'telegram'.
+    #                                       (Для примера сделал так, что все сообщения на whatsapp будут
+    #                                        получать статус failed в очереди).
+    #    message/consumers_info/time_to_deliver - timestamp - integer. Опциональный параметр, время, когда сообщение
+    #                                             должно быть доставлено. Если время меньше текущего, то сообщение
+    #                                             отправляется незамедлительно.
+    #
+    # Ответ
+    #   В случае валидного запроса:
+    #   Результат: Массив из созданных сообщений в системе в том порядке, в котором они были указаны при запросе
+    #   Пример:
+    #   {
+    #     "status": "accepted",
+    #     "ids": [1, 2]
+    #   }
+    #
+    #   В случае, невалидного запроса
+    #   Результат: Хэш с ошибками в формате:
+    #    {"reasons": {"body": [массив строк с описанием ошибок],
+    #                 "consumer_info":
+    #                   { Порядковый номер сообщения в запросе: {невалидное поле: [массив строк с описанием ошибок]]}}}
+    #                }
+    #    }
+    #
+    #   Пример:
+    #   {
+    #    "status": "invalid_data",
+    #      "reasons": {
+    #          "body": [
+    #              "must be filled",
+    #              "length must be within 1 - 500"
+    #          ],
+    #          "consumers_info": {
+    #              "2": {
+    #                  "messenger": [
+    #                      "must be one of: telegram, whats_app, viber"
+    #                  ]
+    #              }
+    #          }
+    #      }
+    #  }
+    #
     def create
       Messages::CreatingBatch.new.call(create_params) do |res|
         res.success do |messages|
@@ -14,6 +90,8 @@ module Api
       end
     end
 
+    # GET /api/messages/:id
+    # Возращает запись Message в json-формате
     def show
       render json: Message.find(params[:id]), status: :ok
     end
